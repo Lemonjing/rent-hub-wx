4ff59dd4dc989704577972dd451c9604e9f4d81f
、Campanulaceae <xmusaber@163.com>
2017-05-20 16:15:32 +0800

    add

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -4,7 +4,8 @@ var app = getApp()
 
 Page({
   data: {
-    info: { id: 1, title: "AAA", img: "/images/settings-81-filled.png", time: "2017-5-1", content: "neirong"}
+    info: {
+      id: 1, title: "浦东川沙精装出租，可押一付一直达张江高科", image: ["https://img3.doubanio.com/view/group_topic/large/public/p77157214.jpg", "https://img3.doubanio.com/view/group_topic/large/public/p77157215.jpg"], time: "2017-5-1", content: "浦东二号线川沙地铁站，玉兰苑，妙境家园，妙境三村，一村，二村，妙栏小区，曙光苑，华川家园，馨汇佳苑，川虹新苑，单间，一室，两室，三室都有，步行100米到2000米皆有☞都是精装修，可以押一付一哦，这边新开的百联购物中心可以满足你的购物欲望，生活家居用品家乐福也是首选，川沙步行街是happy的好地方，直达张江，浦东机场，陆家嘴的朋友也可以来这边主要是便宜，真便宜，价格1000到5000都有，看你租什么样的啦，有需要联系18516183933，手机号这是微信号"}
   },
 
   bindViewTap: function () {
